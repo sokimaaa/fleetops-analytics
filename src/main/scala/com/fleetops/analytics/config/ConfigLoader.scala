540021fa5c63a4784a258335ea0eb0a8b64c43a7
sokimaaa <romandenysov9@gmail.com>
2026-03-07 18:58:27 +0200

    [FOA-3] implement ConfigLoader

--- src/main/scala/com/fleetops/analytics/config/ConfigLoader.scala
+++ src/main/scala/com/fleetops/analytics/config/ConfigLoader.scala
@@ -2,22 +2,34 @@ package com.fleetops.analytics.config
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-object ConfigLoader {
-  private val RootPath = "fleetops"
+trait ConfigLoader[C] {
+  def load(config: Config): C
+}
 
+object ConfigLoader {
   def load(): AppConfig = {
     val config = ConfigFactory.load()
-    load(config)
+    FleetOpsConfigLoader.load(config)
   }
+}
+
+object FleetOpsConfigLoader extends ConfigLoader[AppConfig] {
+  private val RootPath = "fleetops"
 
   def load(config: Config): AppConfig = {
     val fleetopsConfig = config.getConfig(RootPath)
     AppConfig(
-      storage = loadStorageConfig(fleetopsConfig.getConfig("storage")),
-      dataset = loadDatasetConfig(fleetopsConfig.getConfig("dataset")),
-      spark = loadSparkConfig(fleetopsConfig.getConfig("spark"))
+      storage = StorageConfigLoader.load(fleetopsConfig),
+      dataset = DatasetConfigLoader.load(fleetopsConfig),
+      spark = SparkConfigLoader.load(fleetopsConfig)
     )
   }
+}
+
+object StorageConfigLoader extends ConfigLoader[StorageConfig] {
+  private val StoragePath = "storage"
+
+  override def load(config: Config): StorageConfig = loadStorageConfig(config.getConfig(StoragePath))
 
   private def loadStorageConfig(config: Config): StorageConfig =
     StorageConfig(
@@ -26,6 +38,12 @@ object ConfigLoader {
       silver = config.getString("silver"),
       gold = config.getString("gold")
     )
+}
+
+object DatasetConfigLoader extends ConfigLoader[DatasetConfig] {
+  private val DatasetPath = "dataset"
+
+  override def load(config: Config): DatasetConfig = loadDatasetConfig(config.getConfig(DatasetPath))
 
   private def loadDatasetConfig(config: Config): DatasetConfig =
     DatasetConfig(
@@ -33,6 +51,12 @@ object ConfigLoader {
       year = config.getInt("year"),
       month = config.getInt("month")
     )
+}
+
+object SparkConfigLoader extends ConfigLoader[SparkConfig] {
+  private val SparkPath = "spark"
+
+  override def load(config: Config): SparkConfig = loadSparkConfig(config.getConfig(SparkPath))
 
   private def loadSparkConfig(config: Config): SparkConfig =
     SparkConfig(
